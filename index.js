a3f124e3c8238825e43ee58f1a7a0b720c326f9f
Xinyuan Wang <xinyuan@narvii.com>
2017-09-15 12:04:27 +0800

    update
    - bump dot version
    - pass query as templateSettings

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var dot = require('dot');
 var fs = require('fs');
+var utils = require('loader-utils');
 
 module.exports = function(content) {
   if (this.cacheable) {
     this.cacheable();
   }
   
-  dot.templateSettings.selfcontained = true;
+  var settings = Object.assign({}, dot.templateSettings, {
+    selfcontained: true
+  }, utils.parseQuery(this.query));
 
   var content = fs.readFileSync(this.resourcePath);
-  return "module.exports = " + dot.template(content);
+
+  return "module.exports = " + dot.template(content, settings);
 };
